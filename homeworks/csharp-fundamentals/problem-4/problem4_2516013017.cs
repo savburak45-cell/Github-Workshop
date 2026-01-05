5fe5e093c67bcc2388781bc1a3a4afe708b297e9
savburak45-cell <savburak45@gmail.com>
2026-01-05 12:48:25 +0300

    Create problem4_2516013017.cs

---
+++ homeworks/csharp-fundamentals/problem-4/problem4_2516013017.cs
@@ -0,0 +1,99 @@
+using System;
+using System.Collections.Generic;
+
+namespace CSharpHomework
+{
+    public class Problem4
+    {
+        public static int DiziToplami(int[] dizi)
+        {
+            int toplam = 0;
+            
+            foreach (int sayi in dizi)
+            {
+                toplam += sayi;
+            }
+
+            return toplam;
+        }
+
+        public static double DiziOrtalamasi(int[] dizi)
+        {
+            if (dizi.Length == 0)
+                return 0;
+
+            double toplam = 0;
+
+            foreach (int sayi in dizi)
+            {
+                toplam += sayi;
+            }
+
+            return toplam / dizi.Length;
+        }
+
+        public static int EnBuyukBul(int[] dizi)
+        {
+            if (dizi.Length == 0) return 0;
+
+            int enBuyuk = dizi[0];
+
+            foreach (int sayi in dizi)
+            {
+                if (sayi > enBuyuk)
+                {
+                    enBuyuk = sayi;
+                }
+            }
+
+            return enBuyuk;
+        }
+
+        public static int EnKucukBul(int[] dizi)
+        {
+            if (dizi.Length == 0) return 0;
+
+            int enKucuk = dizi[0];
+
+            foreach (int sayi in dizi)
+            {
+                if (sayi < enKucuk)
+                {
+                    enKucuk = sayi;
+                }
+            }
+
+            return enKucuk;
+        }
+
+        public static List<int> CiftSayilariFiltrele(int[] dizi)
+        {
+            List<int> ciftSayilar = new List<int>();
+
+            foreach (int sayi in dizi)
+            {
+                if (sayi % 2 == 0)
+                {
+                    ciftSayilar.Add(sayi);
+                }
+            }
+
+            return ciftSayilar;
+        }
+
+        public static int SayiTekrarSay(int[] dizi, int aranan)
+        {
+            int tekrarSayisi = 0;
+
+            foreach (int sayi in dizi)
+            {
+                if (sayi == aranan)
+                {
+                    tekrarSayisi++;
+                }
+            }
+
+            return tekrarSayisi;
+        }
+    }
+}
